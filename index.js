1d5d30d35be0425e175551507d630714885bad2e
mahfuj-bd <mahfujshakibbd@gmai.com>
2023-10-21 01:51:00 +0600

    added update

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ async function run() {
       res.send(result);
   })
 
+  app.get('/products/:id',async(req,res)=>{
+   const id = req.params.id;
+   console.log(id);
+   const query = {
+    _id : new ObjectId(id)
+   }
+    const result = await dataCollection.findOne(query)
+    console.log(result);
+    res.send(result)
+})
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
